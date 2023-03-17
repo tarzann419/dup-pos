08dcc5eadf0aa3c63d0e19af98f3dd8f14749ced
dantheman <danogbo0@gmail.com>
2023-03-17 22:15:39 +0100

    final commit

--- routes/web.php
+++ routes/web.php
@@ -91,5 +91,6 @@ Route::controller(CategoryController::class)->group(function () {
 Route::controller(ProductController::class)->group(function () {
     Route::get('/all/product', 'AllProduct')->name('all.product');
     Route::get('/add/product', 'AddProduct')->name('add.product');
+    Route::post('/store/product', 'AddProduct')->name('add.product');
     
 });
