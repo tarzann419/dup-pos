2cbedb9534a11fece5e6a839914f560c007ee2bf
dantheman <danogbo0@gmail.com>
2023-03-18 09:25:37 +0100

    edit-delete product commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,6 +11,10 @@ class Product extends Model
     protected $guarded = [];
 
     public function category(){
-        return $this->belongsTo('category_id')
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function supplier(){
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'id');
     }
 }
--- resources/views/backend/product/all_product.blade.php
+++ resources/views/backend/product/all_product.blade.php
@@ -54,13 +54,15 @@
                                     <td>{{ $key+1 }}</td>
                                     <td> <img src="{{ asset($item->product_image) }}" style="width:50px; height: 40px;"> </td>
                                     <td>{{ $item->product_name }}</td>
-                                    <td>{{ $item->category_id }}</td>
-                                    <td>{{ $item->supplier_id }}</td>
+                                    <!-- accessing the specific names after making a relationship between the tables from the Product Model page
+                                                ['functionName']['DBfield'] -->
+                                    <td>{{ $item['category']['category_name'] }}</td>
+                                    <td>{{ $item['supplier']['name'] }}</td>
                                     <td>{{ $item->product_code }}</td>
                                     <td>{{ $item->selling_price }}</td>
                                     <td>
-                                        <a href="" class="btn btn-blue rounded-pill waves-effect waves-light"><i class="fa fa-pencil" aria-hidden="true"></i></a>
-                                        <a href="" class="btn btn-info rounded-pill waves-effect waves-light"><i class="fa fa-barcode" aria-hidden="true"></i></a>
+                                        <a href="{{ route('edit.product', $item->id) }}" class="btn btn-blue rounded-pill waves-effect waves-light"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                        <a href="{{ route('delete.product', $item->id) }}" class="btn btn-info rounded-pill waves-effect waves-light"><i class="fa fa-barcode" aria-hidden="true"></i></a>
                                         <a href="" class="btn btn-danger rounded-pill waves-effect waves-light" id="delete"><i class="fa fa-trash" aria-hidden="true"></i></a>
 
                                     </td>
